02d830c42d1ee42d7e37c4b4749acac8f50db97b
engrreaz <engrreaz@gmail.com>
2024-08-30 16:02:51 +0600

    OK

--- queeditor.php
+++ queeditor.php
@@ -280,7 +280,7 @@ if ($userlevel == 'Super Administrator') {
 
     <script>
         var toggler = document.getElementsByClassName("caret");
-        var i;
+        var i; 
 
         for (i = 0; i < toggler.length; i++) {
             toggler[i].addEventListener("click", function () {
